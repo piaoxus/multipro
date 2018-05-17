d468ae1d5d1f70b3f4449207d93fd910c8839376
zhangxh <406717279@qq.com>
2018-05-17 23:35:20 +0800

    新建分支

--- z-core/src/main/java/Test.java
+++ z-core/src/main/java/Test.java
@@ -2,5 +2,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.print("haha");
         System.out.println("wawa");
+        System.out.println("xixixi");
     }
 }
