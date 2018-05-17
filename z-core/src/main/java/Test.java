755a1b7c890f0aa4e0e62db51f88176996966cf4
zhangxh <406717279@qq.com>
2018-05-17 23:59:22 +0800

    提交到dev分支3

--- z-core/src/main/java/Test.java
+++ z-core/src/main/java/Test.java
@@ -4,5 +4,8 @@ public class Test {
         System.out.println("wawa");
         System.out.println("xixixi");
         System.out.println("张三");
+        System.out.println("张三");
+        System.out.println("张三");
+        System.out.println("张三");
     }
 }
